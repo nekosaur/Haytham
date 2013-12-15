89cc9db68e2ca8f73e4f9375f002002f755fee61
dmardanbegi <dmardanbegi@e4e0c341-fc7d-4f49-a83c-1b0cc5b6c6fc>
2013-12-15 13:38:12 +0000

    detecting the server IP automatically

--- trunk/Haytham_Clients/Haytham_RecipeDemo/Form1.cs
+++ trunk/Haytham_Clients/Haytham_RecipeDemo/Form1.cs
@@ -8,16 +8,50 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+
+using System.Threading;
+using Haytham.ExtData;	//change @PJ
+
 namespace Haytham_Client
 {
     public partial class Form1 : Form
     {
+        private IPAddress serverip;
+
         public Form1()
         {
             InitializeComponent();
 
             Icon ico = new Icon(Properties.Resources.Untitled_2, 64, 64);
             this.Icon = ico;
+            //@PJ
+            //start server search task using haytham extData client		
+            System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                //find haytham hosts on network					
+                Uri hostUri = Client.getActiveHosts().FirstOrDefault();
+                while (hostUri == null)
+                {
+                    System.Threading.Thread.Sleep(5000);	//wait 5 seconds before next try
+                    hostUri = Client.getActiveHosts().FirstOrDefault(); // it has 2seconds timeout
+                }
+
+                //show IPv4 address if exists
+                var server = Dns.GetHostAddresses(hostUri.DnsSafeHost).Where(adr => adr.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+                if (server != null)
+                {
+                    this.serverip = server;
+                    showIp();
+                }
+            });
+        }
+
+        private void showIp()
+        {
+            if (this.textBox1.InvokeRequired)
+                Invoke((Action)this.showIp);
+            else
+                this.textBox1.Text = this.serverip.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
