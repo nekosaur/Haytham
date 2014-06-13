cf5a40ca19413d0390521ad1fab7d2836461c656
dmardanbegi <dmardanbegi@e4e0c341-fc7d-4f49-a83c-1b0cc5b6c6fc>
2014-06-13 10:36:19 +0000

    one file was missing.

---
+++ trunk/HaythamServer/Haytham_Server/Haytahm.ExtData/Service/CalibrationMethod.cs
@@ -0,0 +1,18 @@
+﻿namespace Haytham.ExtData.Service
+{
+  /// <summary>
+  ///   The both available calibration methods
+  /// </summary>
+  public enum CalibrationMethod
+  {
+    /// <summary>
+    ///   Homography 4 point calibration
+    /// </summary>
+    Point4,
+
+    /// <summary>
+    ///   Polynomial 9 point calibration
+    /// </summary>
+    Point9
+  }
+}
