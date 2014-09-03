e21df7696b7731487bae7b5ce7097dff68cba4db
dmardanbegi <dmardanbegi@e4e0c341-fc7d-4f49-a83c-1b0cc5b6c6fc>
2014-09-03 17:15:08 +0000

    delete

--- branches/HaythamServer/_ compile.bat
+++
@@ -1,6 +0,0 @@
-REM set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v3.5
-set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %msBuildDir%\msbuild.exe  Haytham_Server\Haytham.sln /t:Clean,Build /p:Configuration=Release /p:Platform="Any CPU" /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
-set msBuildDir=
-
-pause
--- branches/HaythamServer/_clean.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-FOR /R . %%X IN (obj) DO (RD /S /Q "%%X" 2>nul)
-FOR /R . %%X IN (Debug) DO (RD /S /Q "%%X" 2>nul)
