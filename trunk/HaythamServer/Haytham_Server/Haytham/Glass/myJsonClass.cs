63b8cf5ebd45bf8022942ffdeafda4c32154c27f
dmardanbegi <dmardanbegi@e4e0c341-fc7d-4f49-a83c-1b0cc5b6c6fc>
2014-09-23 22:31:23 +0000

    -

---
+++ trunk/HaythamServer/Haytham_Server/Haytham/Glass/myJsonClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+
+
+namespace myGlass
+{
+
+
+    [DataContract]
+    //[Serializable()]
+    public  class myJsonClass
+    {
+        [DataMember(Name = "name", IsRequired = false)]
+        public string name { get; set; }
+
+        [DataMember(Name = "image", IsRequired = false)]
+        public string image
+        {
+            set
+            {
+
+                byte[] array = Convert.FromBase64String(value);
+
+                System.IO.MemoryStream dataOutputStream = new System.IO.MemoryStream();
+                dataOutputStream.Write(array, 0, array.Length);
+                img = Image.FromStream(dataOutputStream);
+
+
+                //byte[] array = Encoding.ASCII.GetBytes(value);
+
+                //ImageConverter ic = new ImageConverter();
+                //img = (Image)ic.ConvertFrom(array);
+
+
+
+            }
+        }
+
+        [DataMember(Name = "texts", IsRequired = false)]
+        public List<String> texts { get; set; }
+
+        [DataMember(Name = "text", IsRequired = false)]
+        public String text { get; set; }
+
+        public Image img;
+
+        public myJsonClass()
+        {
+            img = null;
+            texts = new List<string>();
+            text = "";
+        }
+    }
+
+
+
+    public class myJsonClass_test
+    {
+
+        public string name { get; set; }
+        public String text { get; set; }
+
+
+        public myJsonClass_test(myJsonClass mjson)
+        {
+            name = mjson.name;
+            text = mjson.text;
+        }
+
+    }
+}
